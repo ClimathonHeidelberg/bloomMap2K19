8cb10f2b776835197e2db72f9ebe6b3aa79eb228
christian <christian@flavigny.de>
2019-10-27 02:35:26 +0200

    move map stuff to django

--- bloomhunt/map/views.py
+++ bloomhunt/map/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+import json
+import os
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if not os.path.isfile("/tmp/data.json"):
+        data = {"trees": str({ "trees": []}) }
+    else:
+        with open("/tmp/data.json", "r") as f:
+            loaded = json.load(f)
+            print("loaded", loaded)
+            s = json.dumps(loaded);
+            print("s", s)
+            data = {"trees": s}
+    return render(request, "map/index.html", context=data);
